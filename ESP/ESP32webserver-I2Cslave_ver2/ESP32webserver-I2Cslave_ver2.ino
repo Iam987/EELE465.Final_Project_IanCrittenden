c89d070ef3c2e67b242b4ff8f24cae6c088dd6d8
Ian Crittenden <iancrittenden@rocketmail.com>
2024-04-28 17:30:40 -0600

    Format changes

--- ESP/ESP32webserver-I2Cslave_ver2/ESP32webserver-I2Cslave_ver2.ino
+++ ESP/ESP32webserver-I2Cslave_ver2/ESP32webserver-I2Cslave_ver2.ino
@@ -102,7 +102,7 @@ void handle_NotFound(){
 String SendHTML(){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>LED Control</title>\n";
+  ptr +="<title>Home Status</title>\n";
   ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
   ptr +=".button {display: block;width: 80px;background-color: #29c900;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
