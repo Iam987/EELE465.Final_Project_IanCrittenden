b03654206e51ed2bcb8c919cb281bfbb32888ce9
Ian Crittenden <iancrittenden@rocketmail.com>
2024-04-29 17:26:36 -0600

    Hopfully add NRF receiver functionallity to slave

--- Slave/main.c
+++ Slave/main.c
@@ -82,3 +82,254 @@ void Write_Byte_MSB_First(unsigned char content[], int index2);
 void Write_Payload_MSB_First(int pyld[], int index3);
 void ser_output(char *str);  //Serial output func
 
+void main(void)
+    {
+
+    __delay_cycles(100); //power on reset
+
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    //P1DIR &= 0x00 ;
+    P1OUT &= 0x00;
+    P1DIR |= MOSI + SCLK + CE + CSN ;  //Output Pins
+    P1DIR &= ~MISO;
+    CE_Off();
+    CSN_On();
+    /************************
+    **CONFIGURING REGISTERS**
+    *************************/
+    //EN_AA  -- enabling AA in all pipes
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | EN_AA);
+    Write_Byte_MSB_First(en_aa_register,1);
+    CSN_On();
+    //RF_SETUP
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RF_SETUP);
+    Write_Byte_MSB_First(rf_setupregister,1);
+    CSN_On();
+    //RX_ADDR_P0
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_ADDR_P0);
+    Write_Byte_MSB_First(address,5); // write 5 bytes address
+    CSN_On();
+    //TX_ADDR
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | TX_ADDR);
+    Write_Byte_MSB_First(address,5); // write 5 bytes address
+    CSN_On();
+    //RF_CH
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RF_CH);
+    Write_Byte_MSB_First(rf_chanregister,1);
+    CSN_On();
+    //SETUP_RETR
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | SETUP_RETR);
+    Write_Byte_MSB_First(setup_retr_register,1);
+    CSN_On();
+    //RX_PW0
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P0);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //RX_PW1
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P1);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //RX_PW2
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P2);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //RX_PW3
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P3);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //RX_PW4
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P4);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //RX_PW4
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | RX_PW_P5);
+    Write_Byte_MSB_First(rx_pw_register,1);
+    CSN_On();
+    //CONFIG
+    CSN_Off();
+    Instruction_Byte_MSB_First(W_REGISTER | CONFIG);
+    Write_Byte_MSB_First(configregister,1);
+    CSN_On();
+    /****************************
+    **END CONFIGURING REGISTERS**
+    *****************************/
+    __delay_cycles(2000);  //start_up >1.5 ms
+
+    while(1){
+        CE_On();
+        __delay_cycles(150); //settling RX
+        CSN_Off();
+        Instruction_Byte_MSB_First(NOP);  //to get the status reg..
+        CSN_On();
+        if ((status_reg & BIT6) == 0x40){
+            CE_Off();
+            CSN_Off();
+            Instruction_Byte_MSB_First(R_RX_PAYLOAD);
+            Read_Byte_MSB_First(32,read_PAYLOAD);
+            CSN_On();
+            pipe_nr = status_reg & BIT4;
+            ltoa(pipe_nr,pipe_nr_chr);
+            //ser_output(pipe_nr_chr);
+            //ser_output(next_satir);
+            j=0;
+            l=0;
+            for (i=0;i<=14;i+=2){
+                pyld1[j]=read_PAYLOAD[i] | (read_PAYLOAD[i+1] << 8);
+                ltoa(pyld1[j],buf);
+                ser_output(buf); ser_output(bosluk);
+                j++;
+
+            }
+            ser_output(next_satir);
+            for (i=16;i<=30;i+=2){
+                            pyld2[l]=read_PAYLOAD[i] | (read_PAYLOAD[i+1] << 8);
+                            ltoa(pyld1[j],buf);
+                            //ser_output(buf); ser_output(bosluk);
+                            l++;
+                        }
+            //ser_output(next_satir);
+            CSN_Off();
+            Instruction_Byte_MSB_First(W_REGISTER | STATUS);
+            Write_Byte_MSB_First(clr_status,1);
+            CSN_On();
+        }
+
+    }
+}
+void SCLK_Pulse (void)
+{
+  P1OUT |= SCLK;//set high with OR 1
+  P1OUT ^= SCLK;//toggle with XOR 1
+}
+void Send_Bit (unsigned int value)
+{
+    if (value != 0){
+        P1OUT |= MOSI;}
+    else {
+        P1OUT &= ~MOSI;
+    }
+}
+void CE_On (void)
+{
+    P1OUT |= CE;
+}
+
+void CE_Off (void)
+{
+    P1OUT &= ~CE;
+}
+void CSN_On (void)
+{
+    P1OUT |= CSN;
+}
+void CSN_Off (void)
+{
+    P1OUT &= ~CSN;
+}
+void Write_Byte(int content)  //Not ued in this application
+{
+
+    for (j=0;j<8;j++){
+             x = (content & (1 << j));  //Write to Address
+             Send_Bit(x);
+             SCLK_Pulse();
+        }
+}
+void Instruction_Byte_MSB_First(int content)
+{
+
+    for (k=7;k>=0;--k){
+             x = (content & (1 << k));  //Write to Address
+             status_reg <<= 1;
+             Send_Bit(x);
+
+             if ((P1IN & MISO) == 0x02){
+
+
+                                             status_reg |= 0b00000001;
+                                                }
+                                                else {
+
+
+                                             status_reg  &= 0b11111110;
+                                                }
+
+             SCLK_Pulse();
+
+                         }
+
+}
+void Read_Byte_MSB_First(int index, unsigned char regname[])
+{
+    for (i=0;i<=(index-1);i++){
+        for (k=0;k<8;k++){
+           regname[i] <<= 1;
+
+
+                     if ((P1IN & MISO) == 0x02){
+
+                                                 //read_reg |= 0b10000000;
+                                                   regname[i] |= 0b00000001;
+                                                    }
+                                                    else {
+
+                                                 //read_reg  &= 0b01111111;
+                                                   regname[i]  &= 0b11111110;
+                                                    }
+                     SCLK_Pulse();
+    }
+}
+}
+void Write_Byte_MSB_First(unsigned char content[], int index2)
+{
+    for (i=0;i<=(index2-1);i++){
+    for (k=7;k>=0;--k){
+
+             x = (content[i] & (1 << k));  //Write to Address
+             Send_Bit(x);
+             SCLK_Pulse();
+
+                         }
+
+}
+}
+
+void Write_Payload_MSB_First(int pyld[], int index3)
+{
+    for (pd_i=0;pd_i<=(index3-1);pd_i++){
+        for (pd=7;pd>=0;--pd){
+
+                     pd_x = (pyld[pd_i] & (1 << pd));  //Write to Address
+                     Send_Bit(pd_x);
+                     SCLK_Pulse();
+
+                                 }
+        for (pd=15;pd>=8;--pd){
+
+                             pd_x = (pyld[pd_i] & (1 << pd));  //Write to Address
+                             Send_Bit(pd_x);
+                             SCLK_Pulse();
+
+                                         }
+
+        }
+}
+void ser_output(char *str){
+    while(*str != 0){
+        while (!(IFG2&UCA0TXIFG));
+        Received_instruction = *str++;
+    }
+}
+
